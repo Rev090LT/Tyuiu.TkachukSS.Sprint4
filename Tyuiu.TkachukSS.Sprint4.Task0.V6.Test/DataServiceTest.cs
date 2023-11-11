875cd8a90778de0e90a6a3bc5a81baab4fa6d217
Rev090LT <mamontosaimon900@gmail.com>
2023-11-11 12:03:52 +0500

    Add project files.

---
+++ Tyuiu.TkachukSS.Sprint4.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TkachukSS.Sprint4.Task0.VLib;
+namespace Tyuiu.TkachukSS.Sprint4.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int[] nums = { 8, 9, 5, 4, 3, 2, 1, 0, 5, 7 };
+            int res = dataService.GetMultOddArrEl(nums), wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TkachukSS.Sprint4.Task0.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TkachukSS.Sprint4.Task0.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TkachukSS.Sprint4.Task0.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00b59886-b409-4e07-95a6-ea971b10a4c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
