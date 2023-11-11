dc158018ea55d3c346865154b668acad77c9203b
Rev090LT <mamontosaimon900@gmail.com>
2023-11-11 17:22:10 +0500

    Task1

---
+++ Tyuiu.TkachukSS.Sprint4.Task1.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TkachukSS.Sprint4.Task1.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TkachukSS.Sprint4.Task1.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("442d0a7b-9ed8-417d-aa03-73ad55d8fd5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
